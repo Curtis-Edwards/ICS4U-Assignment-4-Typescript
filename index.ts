b7821b77504ad7dcec31a0a47373e4f0203cb61e
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-05-08 16:44:50 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@
 // This function calculates the palindrome depth of a number
 function PalindromeDepth(integer: number, currentDepth: number): number {
   const reversedInteger: number = integer.toString().split('').reverse().join('')
+  console.log("${reversedInteger} and ${integer}")
   if (integer === reversedInteger) {
     return currentDepth
   } else {
