946cb9cc647ea64e804f9c530128febb43e44c3e
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-05-08 16:54:27 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,9 +6,13 @@
  * @since   2024-05-08
  */
 
-// This function calculates the palindrome depth of a number
+/*
+ * This function calculates the palindrome depth of a number
+ */
 function PalindromeDepth(integer: number, currentDepth: number): number {
+  // reverse the integer
   const reversedInteger: number = integer.toString().split('').reverse().join('')
+  // check if integer is a palindrome
   if (integer == reversedInteger) {
     return currentDepth
   } else {
@@ -18,10 +22,10 @@ function PalindromeDepth(integer: number, currentDepth: number): number {
 }
 
 
-const minInt: number = 10
-const maxInt: number = 99
+const startInt: number = 10
+const finishInt: number = 99
 
-for (let counter = minInt; counter <= maxInt; counter++) {
+for (let counter = startInt; counter <= finishInt; counter++) {
   let intDepth: number = PalindromeDepth(counter, 0)
   console.log(`${counter} is a depth ${intDepth} palindrome.`)
 }
