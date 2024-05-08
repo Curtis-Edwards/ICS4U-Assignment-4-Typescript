e8a9276168df8455ff700ab1e7c3eda8185248d8
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-05-08 16:11:23 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,35 @@
-console.log("Hello via Bun!");
+/*
+ * This is the PalindromeDepth program
+ *
+ * @author  Curtis Edwards
+ * @version 1.0
+ * @since   2024-05-08
+ */
+
+//function PalindromeDepth(Integer: number, currentDepth: number): number {
+  /*
+   * This function calculates the palindrome depth of a number
+   */
+  /*let reversedString = ""
+  let isPalindrome
+  // reverse string
+  for (let counter = string.length - 1; counter >= 0; counter--) {
+    reversedString += string[counter]
+  }
+  // check if string is palindrome (capitalization doesn't matter)
+  if (string.toLowerCase() === reversedString.toLowerCase()) {
+    isPalindrome = true
+  } else {
+    isPalindrome = false
+  }
+
+  return isPalindrome
+}*/
+
+const minInteger: number = 10
+const maxInteger: number = 99
+for (let counter = minInteger; counter <= maxInteger; counter++) {
+  console.log(counter)
+}
+
+console.log("\nDone.")
